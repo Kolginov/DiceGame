a3270fe237c77d52308146ed03f1dabfaf9d7088
Олег <kolginovog@mailru>
2021-11-16 16:08:06 +0300

    first commit

---
+++ app/src/main/java/study/kolginov/dicegame/MainActivity.kt
@@ -0,0 +1,12 @@
+package study.kolginov.dicegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import study.kolginov.dicegame.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
